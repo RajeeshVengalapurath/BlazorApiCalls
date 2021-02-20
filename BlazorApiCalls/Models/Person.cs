82cb408b2e72124cd63ffe1c530c65f719ee4c0a
Rajeesh VP <rajeeshvp@gmail.com>
2021-02-20 16:53:32 +0530

    Add project files.

---
+++ BlazorApiCalls/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApiCalls.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string age { get; set; }
+    }
+}
